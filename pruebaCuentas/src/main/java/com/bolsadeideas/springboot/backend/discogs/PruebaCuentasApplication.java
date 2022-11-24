2eef93da78817810ff3a7322e047b7b3ddfce814
jonathanoleaz <jonathanoleaz@hotmail.com>
2022-11-24 11:13:04 -0600

    Se agregan scripts creacion de BD e insercion de catalogos

---
+++ pruebaCuentas/src/main/java/com/bolsadeideas/springboot/backend/discogs/PruebaCuentasApplication.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.backend.discogs;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PruebaCuentasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PruebaCuentasApplication.class, args);
+	}
+
+}
---
+++ pruebaCuentas/src/test/java/com/bolsadeideas/springboot/backend/discogs/PruebaCuentasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bolsadeideas.springboot.backend.discogs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PruebaCuentasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
